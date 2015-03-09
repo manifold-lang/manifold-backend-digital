deb3e2458d031bfc151db52c5a043a4ba524c470
Murphy Berzish <murphy.berzish@gmail.com>
2015-03-09 19:48:44 -0400

    remove symbol escaping logic

--- src/main/java/org/manifold/compiler/back/digital/smt2/Symbol.java
+++ src/main/java/org/manifold/compiler/back/digital/smt2/Symbol.java
@@ -4,41 +4,12 @@ import java.io.IOException;
 import java.io.Writer;
 
 public class Symbol extends SExpression {
-  // TODO symbol names do not always need to be escaped
-  // so only add |s if it is absolutely necessary
-  
-  private String name;
+  private final String name;
   public String getName() {
     return name;
   }
   
-  private void validateName(String name) {
-    // A symbol is a any sequence of printable ASCII characters (including 
-    // space, tab, and line-breaking characters) except for the 
-    // backslash character \, that starts and ends with | 
-    // and does not otherwise contain |.
-    if (!name.startsWith("|")) {
-      throw new IllegalArgumentException("symbol must start with '|'");
-    }
-    if (!name.endsWith("|")) {
-      throw new IllegalArgumentException("symbol must end with '|'");
-    }
-    if (name.indexOf('\\') != -1) {
-      throw new IllegalArgumentException("symbol cannot contain '\\'");
-    }
-  }
-  
   public Symbol(String name) {
-    /*
-    // If the name isn't delimited by |s, add them.
-    if (!name.startsWith("|")) {
-      name = "|" + name;
-    }
-    if (!name.endsWith("|")) {
-      name = name + "|";
-    }
-    validateName(name);
-    */
     this.name = name;
   }
   
